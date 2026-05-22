fcc76e1adb7d6fdbe5f25c84ff0ee704c44b444c
PlasticNerual <tasnimsakibrafi@gmail.com>
2026-05-23 02:08:53 +0600

    Refactor and clean up student marks program

--- Ans_of_Q1mid.cpp
+++ Ans_of_Q1mid.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+
 int main() {
     int n, searchMark;
     cout << "Enter number of students: ";
@@ -11,14 +15,16 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> marks[i];
 
-    // Selection Sort in descending order
+
+    
+ 
     for (int i = 0; i < n - 1; i++) {
         int maxIndex = i;
         for (int j = i + 1; j < n; j++) {
             if (marks[j] > marks[maxIndex])
                 maxIndex = j;
         }
-        // Swap
+     
         int temp = marks[i];
         marks[i] = marks[maxIndex];
         marks[maxIndex] = temp;
@@ -27,13 +33,13 @@ int main() {
     cout << "Enter mark to search: ";
     cin >> searchMark;
 
-    // Binary search for last occurrence (manual)
+   
     int low = 0, high = n - 1, result = -1;
     while (low <= high) {
         int mid = (low + high) / 2;
         if (marks[mid] == searchMark) {
-            result = mid;      // store index
-            low = mid + 1;     // continue right side
+            result = mid;    
+            low = mid + 1;    
         } else if (marks[mid] < searchMark)
             high = mid - 1;
         else
@@ -45,13 +51,15 @@ int main() {
         cout << marks[i];
         if (i != n - 1) cout << ", ";
     }
-    cout << "]" << endl;
+    cout << "]";
+    cout<< endl;
 
     if (result != -1)
         cout << "Last occurrence of " << searchMark << " is at index = " << result << endl;
-    else
-        cout << "Mark not found!" << endl;
-
+    else {
+        cout << "Mark not found!";
+         cout << endl;
+    }
     delete[] marks;
     return 0;
 }
